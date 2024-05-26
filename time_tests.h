c1d801c4d340283cd99117bea22d8ae0d591fc67
gllrmoliva <gllrmoliva42@gmail.com>
2024-05-26 19:54:10 -0400

    intentando hacer pruebas

--- time_tests.h
+++ time_tests.h
@@ -8,6 +8,34 @@
 #include "functions.h"
 #include <numeric>
 
+void test_insert_Username_Open(vector<User> &users, OpenHashTableUserName &HashTable, int N)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    for (User user : users)
+    {
+        HashTable.insert(user.userName, user);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> duration = end - start;
+
+    std::cout << "Tiempo de ejecución: " << duration.count() << " segundos." << std::endl;
+}
+
+void test_insert_Username_Close(vector<User> &users, CloseHashTableUserName &HashTable, int N)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    for (User user : users)
+    {
+        HashTable.insert(user.userName, &user);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> duration = end - start;
+
+    std::cout << "Tiempo de ejecución: " << duration.count() << " segundos." << std::endl;
+}
+
 using namespace std;
 
 #endif
